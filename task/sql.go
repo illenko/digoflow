7c6a80ddfadb823956449af59a298ecab7208027
illenko <kostiaillenko@gmail.com>
2024-06-06 10:06:22 +0300

    SQL Select

--- task/sql.go
+++ task/sql.go
@@ -1,15 +1,69 @@
 package task
 
-import "github.com/illenko/digoflow/container"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/illenko/digoflow/container"
+)
 
 type SQL struct{}
 
 func (t *SQL) Execute(c *container.Container, input Input) (Output, error) {
-	_, err := c.Database.Exec(input["query"].(string))
 
-	if err != nil {
-		return nil, err
+	query := input["query"].(string)
+
+	if query == "" {
+		return nil, fmt.Errorf("query is required")
+	}
+
+	if strings.HasPrefix(query, "SELECT") {
+		rows, _ := c.Database.Query(query)
+		cols, _ := rows.Columns()
+		colsTypes, _ := rows.ColumnTypes()
+
+		if rows.Next() {
+			columns := make([]interface{}, len(cols))
+			columnPointers := make([]interface{}, len(cols))
+			for i := range columns {
+				columnPointers[i] = &columns[i]
+			}
+
+			if err := rows.Scan(columnPointers...); err != nil {
+				return Output{}, err
+			}
+
+			m := make(Output)
+			for i, colName := range cols {
+
+				colType := colsTypes[i]
+
+				val := columnPointers[i].(*interface{})
+
+				switch colType.DatabaseTypeName() {
+				case "JSONB", "UUID":
+					m[colName] = string((*val).([]byte))
+				default:
+					m[colName] = *val
+				}
+			}
+
+			return m, nil
+
+		}
+	} else {
+
+		res, err := c.Database.Exec(input["query"].(string))
+
+		fmt.Println(res)
+
+		if err != nil {
+			return nil, err
+		}
+
+		return Output{}, nil
 	}
 
 	return Output{}, nil
+
 }
